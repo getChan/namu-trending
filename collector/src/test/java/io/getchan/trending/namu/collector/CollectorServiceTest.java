cb1db940ca5a7d83fd519f295edfb7041f4ecc3c
warm-inf <warm.inf@kakaoenterprise.com>
2022-03-25 14:01:31 +0900

    [1-spring-cloud-stream] duplicate test

--- collector/src/test/java/io/getchan/trending/namu/collector/CollectorServiceTest.java
+++ collector/src/test/java/io/getchan/trending/namu/collector/CollectorServiceTest.java
@@ -1,18 +1,15 @@
 package io.getchan.trending.namu.collector;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
 class CollectorServiceTest {
 
-    @Autowired
-    CollectorService collectorService;
+    CollectorService collectorService = new CollectorService(WebClient.builder().build(), new LastCollectedList(15), 5);
 
     @Test
     void requestSidebarDTO() {
@@ -24,8 +21,11 @@ class CollectorServiceTest {
 
     @Test
     void collect() {
-        collectorService.collect();
-        assertThat(collectorService.collect().count()).isLessThan(15);
+        final long first = collectorService.collect().count();
+        System.out.println("first = " + first);
+        final long count = collectorService.collect().count();
+        System.out.println("count = " + count);
+        assertThat(count).isLessThan(15);
     }
 
 }
